36aebfb9382d50de41ee15809370665fb146a968
Doogie13 <jay.finbow13@gmail.com>
2021-08-09 14:34:47 +0100

    long jump naem

--- src/main/kotlin/LongJump.kt
+++ src/main/kotlin/LongJump.kt
@@ -5,9 +5,9 @@ import com.lambda.client.plugin.api.PluginModule
 import com.lambda.client.util.threads.safeListener
 
 internal object LongJump : PluginModule(
-    name = "ModuleExample",
+    name = "LongJump",
     category = Category.MISC,
-    description = "LongJump",
+    description = "Jump Longer",
     pluginMain = LongJumpController
 ) {
     private val farSpeed by setting("Speed", 1.0f, 0.0f..10.0f, 0.1f, description = "Long Jump Factor")
