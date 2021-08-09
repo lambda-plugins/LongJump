cff09b35a7d042066b9ec75131967de97ea52799
Doogie13 <jay.finbow13@gmail.com>
2021-08-09 14:32:52 +0100

    long jump

---
+++ src/main/kotlin/LongJump.kt
@@ -0,0 +1,33 @@
+import com.lambda.client.event.events.PlayerTravelEvent
+import com.lambda.client.module.Category
+import com.lambda.client.module.modules.player.LagNotifier
+import com.lambda.client.plugin.api.PluginModule
+import com.lambda.client.util.threads.safeListener
+
+internal object LongJump : PluginModule(
+    name = "ModuleExample",
+    category = Category.MISC,
+    description = "LongJump",
+    pluginMain = LongJumpController
+) {
+    private val farSpeed by setting("Speed", 1.0f, 0.0f..10.0f, 0.1f, description = "Long Jump Factor")
+    private val disableOnRubberband by setting("Rubberband disable", false)
+
+    init {
+        safeListener<PlayerTravelEvent> {
+
+            if (mc.player.motionY == 0.0030162615090425808) {
+
+                mc.player.jumpMovementFactor = farSpeed
+
+            }
+
+            if (LagNotifier.paused && disableOnRubberband) {
+
+                disable()
+
+            }
+
+        }
+    }
+}
